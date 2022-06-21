a890b0d16508a0c9d3421c212827d7a2316c53ab
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2022-06-21 15:09:24 +0800

    Update tut3.py

--- tutorial/tut3.py
+++ tutorial/tut3.py
@@ -1,9 +1,21 @@
 import cv2
+import numpy as np
+ones = np.ones((3,3), np.uint8)
 
-img = cv2.imread('tutorial/unicon.png', cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, (0,0), fx=0.6, fy=0.6)
+original = cv2.imread('tutorial/unicon.png')
+original = cv2.resize(original, (0,0), fx=0.6, fy=0.6)
+gray = cv2.imread('tutorial/unicon.png', cv2.IMREAD_GRAYSCALE)
+gray = cv2.resize(gray, (0,0), fx=0.6, fy=0.6)
+
 
 # cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(original, (7,7), 1.5)
+canny = cv2.Canny(original, 150, 200)
+dilate = cv2.dilate(canny, ones, iterations=1)
 
-cv2.imshow('image', img)
+#cv2.imshow('original', original)
+#cv2.imshow('gray', gray)
+#cv2.imshow('blur', blur)
+cv2.imshow('canny', canny)
+cv2.imshow('dilate', dilate)
 cv2.waitKey(0)
